200f9a12ceb061d5cfd68b504434b439b88ac5ac
emmo2000 <louis.emmerson2000@gmail.com>
2024-10-25 22:17:33 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import GroceryRecipeList from "./components/GroceryRecipeList";
+import './App.css';
+
+function App() {
+
+  return (
+    <section id="grocery-app">
+        <Header />
+        <GroceryRecipeList />
+    </section>
+  )
+}
+
+export default App
